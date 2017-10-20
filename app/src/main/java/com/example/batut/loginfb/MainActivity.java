93a9bf97c77a672ea8f256e71cbfe26748d0af1f
Italo Queiroz <batutista14@gmail.com>
2017-10-19 22:04:54 -0200

    Login Firebase email/senha

--- app/src/main/java/com/example/batut/loginfb/MainActivity.java
+++ app/src/main/java/com/example/batut/loginfb/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.batut.loginfb;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +18,8 @@ import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -29,13 +35,81 @@ import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText txtEmail;
+    private EditText txtSenha;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtEmail = (EditText) findViewById(R.id.email);
+        txtSenha = (EditText) findViewById(R.id.senha);
 
+         /*
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();*/
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button botaoAcessar = (Button) findViewById(R.id.buttonAcessar);
+        botaoAcessar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signInWithEmailAndPassword(txtEmail.getText().toString(), txtSenha.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(!task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this, "Usuario ou senha inválido", Toast.LENGTH_LONG).show();
+                                }else {
+                                    FirebaseUser user = task.getResult().getUser();
+                                    if(user != null){
+                                        Intent acesso = new Intent(MainActivity.this, Acesso.class);
+                                        startActivity(acesso);
+                                        finish();
+                                    }
+                                }
+                            }
+                        });
+
+            }
+        });
+
+        /*
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Intent acesso = new Intent(MainActivity.this, Acesso.class);
+                startActivity(acesso);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "Erro", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });*/
+
+
+
+
+
+
+
+    }
 
 
 }
